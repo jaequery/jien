50ba22bf05333f12a3acd4b83a0b05d8d3a38b5a
James Lee <uptownhr@gmail.com>
2013-11-21 13:35:19 -0800

    removed test admin action

--- application/controllers/AdminController.php
+++ application/controllers/AdminController.php
@@ -207,22 +207,7 @@ class AdminController extends My_Controller {
     }
 
     
-    //scaffolder Asdfasdf start
-    public function asdfasdfsAction(){
-    	$this->view->model = "Asdfasdf";
-    	$this->view->primary = Jien::model($this->view->model)->getPrimary();
-    	$this->view->data = Jien::model($this->view->model)->orderBy("asdfasdf.asdfasdf_id DESC")->withPager($this->params('page', 1))->filter($this->params())->get();
-    }
-
-    public function asdfasdfAction(){
-    	$this->view->model = "Asdfasdf";
-    	$id = $this->params('id');
-    	if($id){
-    		$this->view->data = Jien::model($this->view->model)->get($id);
-    	}
-    }
-    //scaffolder Asdfasdf end
-
+    
 // skeleton - dont remove this line, it's for scaffolding reason //
 
 
