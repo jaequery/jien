e34b4b45a42b436ba142da0c0dfbf77b5e248523
James Lee <uptownhr@gmail.com>
2013-11-21 04:26:09 -0800

    fixed #19.

--- application/controllers/AdminController.php
+++ application/controllers/AdminController.php
@@ -206,6 +206,36 @@ class AdminController extends My_Controller {
     	}
     }
 
+    public function testsAction(){
+    	$this->view->model = "Test";
+    	$this->view->primary = Jien::model($this->view->model)->getPrimary();
+    	$this->view->data = Jien::model($this->view->model)->orderBy("test.test_id DESC")->withPager($this->params('page', 1))->filter($this->params())->get();
+    }
+
+    public function testAction(){
+    	$this->view->model = "Test";
+    	$id = $this->params('id');
+    	if($id){
+    		$this->view->data = Jien::model($this->view->model)->get($id);
+    	}
+    }
+
+    public function test2sAction(){
+    	$this->view->model = "Test2";
+    	$this->view->primary = Jien::model($this->view->model)->getPrimary();
+    	$this->view->data = Jien::model($this->view->model)->orderBy("test2.test2_id DESC")->withPager($this->params('page', 1))->filter($this->params())->get();
+    }
+
+    public function test2Action(){
+    	$this->view->model = "Test2";
+    	$id = $this->params('id');
+    	if($id){
+    		$this->view->data = Jien::model($this->view->model)->get($id);
+    	}
+    }
+
     // skeleton - dont remove this line, it's for scaffolding reason //
+
+
 
 }
